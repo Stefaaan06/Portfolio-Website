a4a8af692b8f54b0835103dc021a2f1d88335777
Stefaaan <ste321fan@gmail.com>
2023-08-18 13:35:30 +0200

    Initial Commit

---
+++ js/slideshow.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(1);
+});
+
+
+let slideIndex = 1;
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
